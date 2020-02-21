c47015ad31edf0ef99c7989ef872212e009594b3
razakhan12 <raza.bb10@gmail.com>
2020-02-21 16:40:13 -0500

    Updates

---
+++ precache-manifest.26899954035b077b200b7bb6cde16d0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb2839ccae771a1740342546b620a804",
+    "url": "/react-stopwatch/index.html"
+  },
+  {
+    "revision": "61de15e9714a66b9991b",
+    "url": "/react-stopwatch/static/css/main.92a11865.chunk.css"
+  },
+  {
+    "revision": "aacdbf4be91ff66bcfdd",
+    "url": "/react-stopwatch/static/js/2.a238f085.chunk.js"
+  },
+  {
+    "revision": "6abd033fcb9cfe63c76339a35e72d0a8",
+    "url": "/react-stopwatch/static/js/2.a238f085.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61de15e9714a66b9991b",
+    "url": "/react-stopwatch/static/js/main.df6101ce.chunk.js"
+  },
+  {
+    "revision": "5e3e79aa5d982488274f",
+    "url": "/react-stopwatch/static/js/runtime-main.7a158a9d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-stopwatch/static/media/logo.5d5d9eef.svg"
+  }
+]);
